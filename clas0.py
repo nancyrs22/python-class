8face28fd263b6d47537c9889d59ef9bfad7cb42
Nancy <nancyrs22@hotmail.com>
2018-03-24 23:59:24 -0500

    clase 2

---
+++ clas0.py
@@ -0,0 +1,11 @@
+# saludo1 = "hello"
+# print (saludo)
+# print("hola")
+
+saludo = input("escribe un saludo: ")
+print(saludo)
+
+age = input("ingresa tu edad: ")
+# age = int(input("ingresa tu edad: ")) tambien puede ser asi para convertir el dato en int
+new_age = int(age) + 50
+print(new_age)
